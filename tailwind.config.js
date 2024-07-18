daece60600a18f501b438630d6fae112c82fe93b
the3plet <soorajp2002@gmail.com>
2024-07-18 07:54:30 +0530

    new heading added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-      fontFamily:{
-        'nav-text':["Mukta","sans-serif"]
-      }
+      fontFamily: {
+        "nav-text": ["Mukta", "sans-serif"],
+        "h1-main": ["Signika", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
